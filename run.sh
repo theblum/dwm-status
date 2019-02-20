e10920b22cde1afd08be9e0ade3e41c636222f2e
Brian Blumberg <blum@disroot.org>
2019-02-20 14:51:05 -0800

    Allow for program arguments.

--- run.sh
+++ run.sh
@@ -3,8 +3,8 @@
 PROGNAME="dwm-status"
 
 PREFIX=
-if [[ $1 = '-d' ]]; then PREFIX="lldb"
-elif [[ $1 = '-v' ]]; then PREFIX="valgrind --leak-check=full"
+if [[ $1 = '-d' ]]; then PREFIX="lldb --"; shift
+elif [[ $1 = '-v' ]]; then PREFIX="valgrind --leak-check=full --"; shift
 fi
 
-$PREFIX ./$PROGNAME
+$PREFIX ./$PROGNAME $@
